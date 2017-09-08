35989ea81b51b91a4ee10f0dc0f207f4a3caccee
Oleh Opalinskyi <oleh.opalinskyi@gamil.com>
2017-09-08 11:28:53 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -15,16 +15,42 @@ $(function() {
         }).done(function(data) {
             var str = "";
             for(var i=0; i<data.length; i++) {
-                str+= '<option data-id="'+ data[i].id +'">' + data[i].name + "</option>";
+                str+= '<option value="'+ data[i].id +'">' + data[i].name + "</option>";
             }
             $("#namePrice").html(str);
         });
         
         $("#sub").click(function() {
+             var cityId = localStorage.getItem("city");
             var date = $("#date").val();
             var time = $("#time").val();
-            var dateTime = new Date(date + "T" + time + "Z");
-            
+            var dateTime = date + "T" + time + "Z";
+            var name = $("#name").val();
+            var tel = $("#tel").val();
+            var address = $("#adress").val();
+            var recivier = $("#adress").val();
+            var price = $("#namePrice").val();
+            var obj = {
+                person: {
+                    fulName: name,
+                    address: address,
+                    phoneNumber: tel,
+                    recivier: recivier,
+                    dateOfBirth: new Date()
+                },
+                recInfo: {
+                    date: "2017-09-07T14:36:42.823Z",
+                    cityId: cityId,
+                    priceId: price
+                }
+            };
+            $.ajax({
+                url: "http://localhost:50612/receptions/create",
+                method: "POST",
+                data: obj
+            }).done(function(data) {
+                console.log(data);
+            })
         });
         
     });
